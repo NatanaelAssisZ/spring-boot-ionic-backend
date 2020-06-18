7d6ea5d147ec021b044a0992db2dc4598b7c97e1
Natanael Assis <natanaelassisz@gmail.com>
2020-06-18 19:30:23 -0300

    Estado e cidade

---
+++ src/main/java/com/natanaelassis/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.natanaelassis.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.natanaelassis.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
