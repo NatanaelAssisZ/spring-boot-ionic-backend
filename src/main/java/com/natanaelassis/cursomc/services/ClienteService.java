10ca899bbf77525f0eb492af3b1338e3d3491e9a
Natanael Assis <natanaelassisz@gmail.com>
2020-06-20 15:57:17 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/natanaelassis/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.natanaelassis.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natanaelassis.cursomc.domain.Cliente;
+import com.natanaelassis.cursomc.repositories.ClienteRepository;
+import com.natanaelassis.cursomc.resources.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
